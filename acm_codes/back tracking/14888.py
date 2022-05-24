ade47c3907d9df8ddaab47007f97f4977b903f4e
swjw14 <swjw13@gmail.com>
2022-05-24 15:27:38 +0900

    rearrange codes

--- acm_codes/back tracking/14888.py
+++ acm_codes/back tracking/14888.py
@@ -1,49 +1,42 @@
+# 연산자 끼워넣기
+# https://www.acmicpc.net/problem/14888
+
+import sys
+from itertools import permutations
+
+input = sys.stdin.readline
+a = ['+', '-', '*', '/']
+
 N = int(input())
-nums = list(map(int, input().split()))
-plus = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
 
-tmp = nums.pop(0)
-max = 0
-min = 1000000000
+yun = list(map(int, input().split()))
+checks = []
+for i in range(4):
+    checks += [a[i]] * yun[i]
 
-l = dict()
-l['+'] = plus[0]
-l['-'] = plus[1]
-l['*'] = plus[2]
-l['//'] = plus[3]
+max_num = -sys.maxsize
+min_num = sys.maxsize
+indexes = [i for i in range(N - 1)]
+for i in permutations(indexes, N - 1):
+    index = 0
+    ans = numbers[0]
+    while index < N - 1:
+        tmp = i[index]
+        index += 1
+        if checks[tmp] == "+":
+            ans += numbers[index]
+        elif checks[tmp] == "-":
+            ans -= numbers[index]
+        elif checks[tmp] == "*":
+            ans *= numbers[index]
+        else:
+            if ans >= 0:
+                ans = ans // numbers[index]
+            else:
+                ans = -((-ans) // numbers[index])
+    max_num = max(max_num, ans)
+    min_num = min(min_num, ans)
 
-
-def math(prev, turn, trash):
-    global max
-    global min
-    if turn == N:
-        return
-    else:
-        for i in trash:
-            if trash[i] != 0:
-                add_num = eval(str(prev) + i + str(nums[turn]))
-                trash[i] -= 1
-                if add_num > max:
-                    max = add_num
-                    # print(str(prev) + i + str(nums[turn]))
-                if add_num < min:
-                    min = add_num
-                math(add_num, turn + 1, trash)
-                # trash[i] += 1
-
-
-            # if plus[i] != 0:
-            #     add_num = eval(str(prev) + l[i] + str(nums[turn]))
-            #     plus[i] -= 1
-            #     if add_num > max:
-            #         max = add_num
-            #         print(str(prev) + l[i] + str(nums[turn]))
-            #     if add_num < min:
-            #         min = add_num
-            #     math(add_num, turn + 1)
-            #     plus[i] += 1
-
-
-math(tmp, 0, l)
-print(max)
-print(min)
+print(max_num)
+print(min_num)
